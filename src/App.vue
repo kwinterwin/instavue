a02200d069f5432fac65902dc32e79c60b7b2be9
Samuseva Anna <samuseva.a.s@mail.ru>
2019-09-15 21:42:40 +0300

    First vue app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="page">
+    <header>
+      <img src="" alt="Logo" />
+      <button class="authorization-button custom-button" role="login">Login</button>
+    </header>
+    <article>
+      <aside></aside>
+      <main>
+        <router-view></router-view>
+      </main>
+      <aside></aside>
+    </article>
+    <footer>
+      <router-link to="/about">About us</router-link>
+      <router-link to="/allPhotos">All photos</router-link>
+      <a>Information</a>
+      <a>Companies</a>
+    </footer>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="aboutUsInformation">
+    <router-link to="/">
+      <h2>About us</h2>
+    </router-link>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisl neque, varius imperdiet
+      tellus non, ornare tincidunt elit. Sed blandit elit sit amet nunc tincidunt venenatis. Praesent
+      nisl lacus, accumsan non facilisis vel, ullamcorper id turpis. Donec cursus eros vel urna
+      posuere, vitae interdum est aliquet. Etiam gravida felis at libero laoreet, in egestas nunc
+      fermentum. In luctus urna vel vestibulum facilisis. Mauris id urna mauris. Vivamus enim turpis,
+      euismod id ligula ac, mollis dictum velit. Aliquam et ex porta, sagittis nulla et, eleifend mi.
+      Cras et euismod nisi, tincidunt eleifend purus.
+      Aenean ultrices accumsan nibh et egestas. Proin in turpis tempus, dignissim neque iaculis,
+      rutrum libero. Nam congue libero ultricies molestie interdum. Proin risus tellus, tristique ut
+      nibh eget, dapibus tempus felis. Quisque vel bibendum neque. Proin eget urna quis lectus
+      pulvinar auctor. Aenean varius est in consectetur hendrerit. Morbi augue nunc, dignissim sit
+      amet placerat id, rutrum eget lacus. Fusce ut gravida quam. Fusce urna orci, convallis sed
+      porttitor ac, vulputate ac nulla. Proin sollicitudin vel metus vitae fringilla. Vivamus eu
+      dignissim augue. Nam molestie enim id nisl congue, eu lacinia diam consectetur. Donec tincidunt
+      justo id sapien vulputate pulvinar.
+      Donec quis arcu magna. Sed ultrices, enim et scelerisque pellentesque, risus ex aliquam quam, ac
+      auctor elit odio sed nisl. Morbi maximus magna gravida metus facilisis, eu pretium lectus
+      tempor. Quisque eget fermentum orci. Pellentesque consectetur feugiat odio, sed suscipit velit
+      aliquam a. Maecenas tempus metus dui, ut tincidunt massa porta id. Maecenas in erat non sem
+      scelerisque cursus in semper dui. Praesent sagittis libero ut ipsum iaculis scelerisque. Donec
+      feugiat maximus erat, ut cursus est posuere viverra. Sed efficitur sed lorem sed semper.
+      Praesent tellus ipsum, volutpat pulvinar nulla sed, tempor gravida tortor. Donec tristique
+      elementum nisl at eleifend. Nullam sagittis, dui nec tincidunt mollis, ante diam gravida lectus,
+      eu vestibulum ipsum eros ut lacus. Aenean sed elit augue.
+      Donec viverra eros ut lorem commodo pretium. Vestibulum ante ipsum primis in faucibus orci
+      luctus et ultrices posuere cubilia Curae; Morbi a aliquet tellus, vel eleifend nisi. Curabitur
+      blandit turpis ante, vitae congue ante varius sit amet. Maecenas auctor est auctor risus
+      efficitur, a imperdiet mi egestas. Suspendisse quis blandit purus. Pellentesque pellentesque
+      tortor nulla, non feugiat ligula interdum eget. Vestibulum ante ipsum primis in faucibus orci
+      luctus et ultrices posuere cubilia Curae; Aliquam mauris diam, viverra sed diam eu, porta
+      fermentum eros.
+      Morbi commodo volutpat dignissim. Morbi eu felis vel eros commodo scelerisque. Vivamus mauris
+      justo, molestie id imperdiet sed, malesuada eget diam. Suspendisse accumsan turpis quis eleifend
+      tristique. Suspendisse viverra facilisis ultricies. Donec ornare tempus nibh, sed blandit ex
+      tincidunt eget. Pellentesque porta quam ut pellentesque dapibus. Quisque dapibus nisl eget
+      mauris vehicula gravida. Aenean nec facilisis enim. Aliquam at leo porttitor, auctor erat eu,
+      ornare elit. Integer consequat elit non metus bibendum, et condimentum dolor pharetra.
+      Vestibulum dignissim ante auctor purus auctor semper.
+    </p>
+  </section>
 </template>
